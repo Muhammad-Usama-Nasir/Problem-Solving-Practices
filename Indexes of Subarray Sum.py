c15498c6d2bfe45a4c4b993f4969590cca9a8b3d
Muhammad Usama Nasir <usaman079@gmail.com>
2024-12-08 05:36:38 +0500

    Problems from 'Geeks for Geeks'

--- Indexes of Subarray Sum.py
+++ Indexes of Subarray Sum.py
@@ -1,18 +1,35 @@
-class IndexesOfSubarraySum:
-    def main():
-        arr = [12, 18, 5, 11, 30, 5, 69]
-        # arr = [19, 23, 15, 6, 6, 2, 28, 2, 2]
-        target = 69
-
-        for i in range(len(arr) - 1):
-            sum = 0
-            for num in arr[i:]:
-                sum = sum + num
-                if sum == target:
-                    print(f"The sum of elements from {i+1} to {arr.index(num)+1} is: {sum}")
-                    return i+1, arr.index(num)+1
-                else:
-                    continue
+# =============================  PROBLEM DESCRIPTION  =================================
+# Given an unsorted array arr containing only non-negative integers, your task is to find a continuous subarray
+# (a contiguous sequence of elements) whose sum equals a specified value target. You need to return the 1-based indices
+# of the leftmost and rightmost elements of this subarray.
 
 
-IndexesOfSubarraySum.main()
+def subArraySum(arr, target):
+
+    # Outer loop to iterate over the whole array
+    for i in range(len(arr)):
+        sum = 0
+        # Inner loop to get each number for sum
+        for j in range(i, len(arr)):
+            # Adding all the previous values in the sum until finding the target value
+            sum += arr[j]
+            if sum == target:
+                return i + 1, j + 1
+
+            # Breaking Early if the sum becomes greater than the target
+            # Solves Time Comlexity Issue 
+            if sum > target:
+                break
+    # In case of No Result, returning -1,'' to ot get any errors 
+    return -1, ''
+
+
+# =============================  DUMMY VALUES TO TEST FOR  =================================
+
+# arr = [12, 18, 5, 11, 30, 5, 69]
+# arr = [19, 23, 15, 6, 6, 2, 28, 2, 2]
+# arr = [12, 18, 5, 11, 30, 5]
+arr = [26, 3, 28, 7]
+target = 52
+
+subArraySum(arr, target)
